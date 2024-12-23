e82e0db3cb925c635b2a3adbd77876181f6b4f15
Marrocco-Simone <marrocco.simone00@gmail.com>
2024-12-23 22:46:32 +0100

    button for pdf download

---
+++ src/components/DownloadPDFButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DownloadPDFButton() {
+  return (
+    <button className="bg-blue-600 text-white rounded p-3 hover:text-blue-200 fixed bottom-0 right-0 m-5">
+      Download as PDF
+    </button>
+  );
+}
