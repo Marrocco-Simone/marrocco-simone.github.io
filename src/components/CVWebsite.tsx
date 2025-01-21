123d48088bc2443702df2103c708063cfa169c93
Marrocco-Simone <marrocco.simone00@gmail.com>
2025-01-21 11:54:01 +0100

    fix year

--- src/components/CVWebsite.tsx
+++ src/components/CVWebsite.tsx
@@ -332,7 +332,7 @@ const CVWebsite = () => {
       {/* Footer */}
       <footer className="bg-gray-800 text-white py-8 mt-12">
         <div className="container mx-auto px-4 text-center">
-          <p>© 2024 Marrocco Simone.</p>
+          <p>© {new Date().getFullYear()} Marrocco Simone.</p>
         </div>
       </footer>
     </div>
