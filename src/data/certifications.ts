8284b11e6424d65f951cea003c90d4416cc6e416
Marrocco M4 <marrocco.simone00@gmail.com>
2025-08-05 13:58:09 +0200

    new certifications

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,6 +2,14 @@ import type { DocumentLink } from "../types";
 
 export const certifications: DocumentLink[] = [
   {
+    text: "The International Symposium on On-Line Testing and Robust System Design 2025 (IOLTS) Certification",
+    href: "https://drive.google.com/file/d/1D7bICPzHj9KXe3CfGe3wuoq3pRi5Arn4/view?usp=sharing",
+  },
+  {
+    text: "2025 IEEE Vehicular Networking Conference (VNC) Certification",
+    href: "https://drive.google.com/file/d/1162d8Oo_NBGmFgrFvBfrXv6qanKUnL_h/view?usp=sharing",
+  },
+  {
     text: "English C1 Cambridge CAE Certification",
     href: "https://drive.google.com/file/d/1r-3B8__UoC5Ko2ETJhd0xASZi8NVOFU0/view?usp=sharing",
   },
